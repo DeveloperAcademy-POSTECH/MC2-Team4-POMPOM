e8b3404f8ee2c5022d12585ae8f3e3c2fc1b029c
J2in <jiin62622@gmail.com>
2022-06-08 14:32:54 +0900

    create jeni view
    create jeni view in test branch

---
+++ POMPOM/POMPOM/Views/JeniView.swift
@@ -0,0 +1,20 @@
+//
+//  JeniView.swift
+//  POMPOM
+//
+//  Created by jiin on 2022/06/08.
+//
+
+import SwiftUI
+
+struct JeniView: View {
+    var body: some View {
+        Text("This is mine")
+    }
+}
+
+struct JeniView_Previews: PreviewProvider {
+    static var previews: some View {
+        JeniView()
+    }
+}
