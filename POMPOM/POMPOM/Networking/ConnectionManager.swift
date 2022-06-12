84b4e56dc3fec0f03183bb26ec3996b937d7c06b
Timo Nam <dwbh7358@gmail.com>
2022-06-12 14:07:39 +0900

    [ADD] 사용자 코드 조회/발급 로직

--- POMPOM/POMPOM/Networking/ConnectionManager.swift
+++ POMPOM/POMPOM/Networking/ConnectionManager.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct ConnectionManager {
+    static private let usersRef = Firestore.firestore().collection("users")
+    
+    static func saveCode(code: String){
+        usersRef.addDocument(data: [
+            "code": code
+        ]) { err in
+            if let err = err {
+                dump("Error adding users 아래 문서: \(err)")
+            }
+        }
+    }
+}
--- POMPOM/POMPOM/UI/CoupleView/ViewModel/CodeViewModel.swift
+++ POMPOM/POMPOM/UI/CoupleView/ViewModel/CodeViewModel.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct CodeViewModel {
+    private static let code: String = initializeCode()
+    
+    static func getCode() -> String { code }
+    
+    static func initializeCode() -> String {
+        // UserDefaults에 이미 code가 있을 때
+        if let defaultCode: String = UserDefaults.standard.string(forKey: "code") {
+            return defaultCode;
+        }
+        // UserDefaults에 code가 없을 때
+        else {
+            let code: String = generateCode(length: 10)
+            // code가 중복되지 않고 saveCode()가 100% 성공적으로 완료된다는 가정
+            ConnectionManager.saveCode(code: code)
+            return code;
+        }
+    }
+    
+    // 길이가 length고, 숫자와 영문 대문자로만 이뤄진 코드 생성 및 반환
+    static func generateCode(length: Int) -> String {
+        let elements = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        return String((0 ..< length).map { _ in elements.randomElement()! })
+    }
+}
