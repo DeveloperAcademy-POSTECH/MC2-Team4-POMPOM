353d37b22890a76d09932bf3e4205afc717262b7
J2in <jiin62622@gmail.com>
2022-06-13 21:34:41 +0900

    [ADD] : bubble 완성
    버블 완성하고 리스트(로직 안짜고 그냥 뷰 와장창 불러서 함)틀 잡기

--- POMPOM/POMPOM/UI/ChatView/MessageListView.swift
+++ POMPOM/POMPOM/UI/ChatView/MessageListView.swift
@@ -7,9 +7,17 @@
 
 import SwiftUI
 
+//코멘트 리스트
 struct MessageListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 15) { //버블간 간격 15
+            //채팅 갯수따라 나와야함 아직 안만듬
+            MessageBubbleView(chatMessage: "There are a lot of premium iOS templates on iosapptemplates.com",
+                              isUserBubble: false)
+            MessageBubbleView(chatMessage: "asdf", isUserBubble: true)
+            MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
+            MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
+        }.padding(16)
     }
 }
 
