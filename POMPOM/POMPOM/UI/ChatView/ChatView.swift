d7cc114c0e4611e1699688076488d09459b18ee7
J2in <jiin62622@gmail.com>
2022-06-13 21:35:02 +0900

    [ADD] : 챗뷰 만드는중
    텍스트필드 디자인 수정중

--- POMPOM/POMPOM/UI/ChatView/ChatView.swift
+++ POMPOM/POMPOM/UI/ChatView/ChatView.swift
@@ -7,10 +7,36 @@
 
 import SwiftUI
 
+
+
+//struct OvalTextFieldStyle: TextFieldStyle {
+//    func _body(configuration: TextField<Self._Label>) -> some View {
+//        configuration
+//            .background()
+//    }
+//}
+
+//하프모달
 struct ChatView: View {
+    @State
+    var comment: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            MessageListView()
+            HStack{
+                Image("TextFieldDecoration")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                TextField("코멘트를 입력하세요", text: $comment).frame(height:47).background(
+                    RoundedRectangle(cornerRadius: 24).fill(Color(UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1.0))))
+            }
+            
+            
+        }
     }
+    
+    
 }
 
 struct ChatView_Previews: PreviewProvider {
