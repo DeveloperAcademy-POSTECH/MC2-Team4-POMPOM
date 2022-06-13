86a08359c2861c93b2a1783000ce3cb93477e3bf
J2in <jiin62622@gmail.com>
2022-06-14 02:11:25 +0900

    [ADD] : TextField 추가
    이름 챗뷰에서 TextFieldView로 바꿈

--- POMPOM/POMPOM/UI/ChatView/MessageListView.swift
+++ POMPOM/POMPOM/UI/ChatView/MessageListView.swift
@@ -10,14 +10,22 @@ import SwiftUI
 //코멘트 리스트
 struct MessageListView: View {
     var body: some View {
-        VStack(spacing: 15) { //버블간 간격 15
-            //채팅 갯수따라 나와야함 아직 안만듬
-            MessageBubbleView(chatMessage: "There are a lot of premium iOS templates on iosapptemplates.com",
-                              isUserBubble: false)
-            MessageBubbleView(chatMessage: "asdf", isUserBubble: true)
-            MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
-            MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
-        }.padding(16)
+        ScrollView{
+            VStack(spacing: 15) {
+                //버블간 간격 15
+                //채팅 갯수따라 나와야함 아직 안만듬
+                MessageBubbleView(chatMessage: "There are a lot of premium iOS templates on iosapptemplates.com",
+                                  isUserBubble: false)
+                MessageBubbleView(chatMessage: "asdf", isUserBubble: true)
+                MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
+                MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
+                MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
+                MessageBubbleView(chatMessage: "asdaaaaaaaaaaadfasdfasdfasdfewsdfasdff", isUserBubble: false)
+                
+                
+                
+            }.padding(16)
+        }
     }
 }
 
