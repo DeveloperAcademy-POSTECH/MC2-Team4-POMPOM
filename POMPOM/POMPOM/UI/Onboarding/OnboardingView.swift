bb967f02fb071a71c21de8d59626b7dcc270c801
Felix <102962238+Felix9971@users.noreply.github.com>
2022-06-17 00:30:00 +0900

    [FIX] OnboardingView -  온보딩 화면이 SE와 mini에는 글자가 짤려서 나오는 것을 확인하여 이미지의 크기를 조정함. (#56)

--- POMPOM/POMPOM/UI/Onboarding/OnboardingView.swift
+++ POMPOM/POMPOM/UI/Onboarding/OnboardingView.swift
@@ -17,6 +17,8 @@ struct OnboardingView: View {
             title: "내일 뭐 입고 올꺼야?",
             message: "커플 인생샷을 남기고 싶은데\n상대방이 무엇이 입고올지 고민인가요?",
             onboardingImage: "CuriousBear",
+            onboardingWidth: Constant.screenWidth * (520 / 390),
+            onboardingHeight: Constant.screenHeight * (255 / 844),
             isLast: false
         ),
         
@@ -24,6 +26,8 @@ struct OnboardingView: View {
             title: "코디 너로 결정!",
             message: "내 옷장에 색상을 넣고\n같이 컬러 조합을 맞춰봐요",
             onboardingImage: "ChooseClothes",
+            onboardingWidth: Constant.screenWidth * (360 / 390),
+            onboardingHeight: Constant.screenHeight * (455 / 844),
             isLast: false
         ),
         
@@ -31,6 +35,8 @@ struct OnboardingView: View {
             title: "함께 맞춰봐요",
             message: "상대방과 연결을 통해\n실시간으로 소통하며 확인해요",
             onboardingImage: "Connection",
+            onboardingWidth: Constant.screenWidth * (320 / 390),
+            onboardingHeight: Constant.screenHeight * (245 / 844),
             isLast: true
         )
     ]
