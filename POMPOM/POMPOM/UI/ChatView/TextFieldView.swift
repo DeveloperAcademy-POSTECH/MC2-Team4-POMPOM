7764e2610dcfa93303f1f6dccf38f4c96980b865
Felix <wonyang22@pos.idserve.net>
2022-06-17 01:35:41 +0900

    [FIX] TextFieldView - 코멘트 입력 시 전송 버튼의 색이 바뀌도록 설정.

--- POMPOM/POMPOM/UI/ChatView/TextFieldView.swift
+++ POMPOM/POMPOM/UI/ChatView/TextFieldView.swift
@@ -27,7 +27,7 @@ struct TextFieldView: View {
             }){
                 Image(systemName: "location.fill")
             }.padding( 18)
-                .foregroundColor(Color(UIColor(red: 192/255, green: 192/255, blue: 196/255, alpha: 1.0)))
+                .foregroundColor(comment.isEmpty ? Color(UIColor(red: 192/255, green: 192/255, blue: 196/255, alpha: 1.0)) : Color(UIColor(red: 255/255, green: 81/255, blue: 0/255, alpha: 1.0)))
         }
         .background(
             RoundedRectangle(cornerRadius: 24).fill(Color(UIColor(red: 242/255, green: 242/255, blue: 247/255, alpha: 1.0))).frame(height: 47))
