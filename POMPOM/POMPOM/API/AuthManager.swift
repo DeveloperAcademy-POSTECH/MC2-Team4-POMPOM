6014609cd11b174bb9ac10cc694e3e0694f5d2cf
Luke_Kong <kongthn@gmail.com>
2022-09-04 14:45:57 +0900

    [FEAT] 회원가입, 로그인, 페어링 메서드 구현

--- POMPOM/POMPOM/API/AuthManager.swift
+++ POMPOM/POMPOM/API/AuthManager.swift
@@ -12,16 +12,84 @@ class AuthManager {
         
     }
     
-    func signUp() {
+    func signUp(userID: String, password: String, deviceToken: String) async throws -> Data {
+        guard userID.count == 36 else {
+            return Data()
+        }
+        
+        guard password.count >= 10 && password.count <= 30  else {
+            return Data()
+        }
+        
+        guard deviceToken.count == 64 else {
+            return Data()
+        }
+        
+        
+        let dic = [
+            "username" : userID,
+            "password" : password,
+            "deviceToken" : deviceToken
+        ]
+        let body = try JSONEncoder().encode(dic)
+        
+        
+        var urlComponents = URLComponents(string: "https://pompom-ada.herokuapp.com/api/signup")
+        guard let url = urlComponents?.url else { return Data() }
+        let defaultSession = URLSession(configuration: .default)
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let (data, _) = try await defaultSession.upload(for: urlRequest, from: body)
+        
+        return data
+    }
+    
+    func Authenticate(userName: String, password: String) async throws -> Data {
+        guard userName.count == 36 else {
+            return Data()
+        }
+        
+        guard password.count >= 10 && password.count <= 30  else {
+            return Data()
+        }
+        
+        let dic = [
+            "username" : userName,
+            "password" : password
+        ]
+        let body = try JSONEncoder().encode(dic)
+        
+        var urlComponents = URLComponents(string: "https://pompom-ada.herokuapp.com/api/authenticate")
+        guard let url = urlComponents?.url else { return Data() }
+        let defaultSession = URLSession(configuration: .default)
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let (data, _) = try await defaultSession.upload(for: urlRequest, from: body)
+        
+        return data
         
     }
     
-    func Authenticate() {
+    func pair(token: String, partnerName: String) async throws -> Data{
+        guard partnerName.count == 36 else { return Data()}
         
-    }
-    
-    func pair() {
+        let dic = [
+            "partnerName" : partnerName
+        ]
+        let body = try JSONEncoder().encode(dic)
         
+        var urlComponents = URLComponents(string: "https://pompom-ada.herokuapp.com/api/pair")
+        guard let url = urlComponents?.url else { return Data() }
+        let defaultSession = URLSession(configuration: .default)
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue(token, forHTTPHeaderField: "Authorization")
+        urlRequest.httpMethod = "POST"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let (data, _) = try await defaultSession.upload(for: urlRequest, from: body)
+        
+        return data
     }
     
     func queryAccount() {
