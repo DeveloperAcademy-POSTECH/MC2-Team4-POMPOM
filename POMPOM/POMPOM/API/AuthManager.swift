bdba1cadf27aa97b8f665b0b9110a90ad0d97a6d
Luke_Kong <kongthn@gmail.com>
2022-09-04 14:17:49 +0900

    [ADD] AuthManager

---
+++ POMPOM/POMPOM/API/AuthManager.swift
@@ -0,0 +1,30 @@
+//
+//  AuthManager.swift
+//  POMPOM
+//
+//  Created by GOngTAE on 2022/09/04.
+//
+
+import Foundation
+
+class AuthManager {
+    func hello() {
+        
+    }
+    
+    func signUp() {
+        
+    }
+    
+    func Authenticate() {
+        
+    }
+    
+    func pair() {
+        
+    }
+    
+    func queryAccount() {
+        
+    }
+}
